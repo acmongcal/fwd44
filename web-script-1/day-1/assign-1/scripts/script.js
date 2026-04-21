3b71d57d0d163d1c0cba702869d65d0922339b60
Alan Christopher Mongcal <aranmongcal@gmail.com>
2026-04-21 10:47:14 -0700

    Added previous lab activities

---
+++ web-script-1/day-1/assign-1/scripts/script.js
@@ -0,0 +1,26 @@
+const output = document.getElementById('output');
+const login = document.getElementById('btn-log-in');
+const logout = document.getElementById('btn-log-out');
+
+login.addEventListener('click', function(){
+    username = window.prompt("What is your name?");
+    output.innerText = `Hello, ${username}. Click the button below to logout`;
+    login.style.display='none';
+    login.setAttribute('disabled','disabled');
+    logout.style.display='block';
+    logout.removeAttribute('disabled');
+});
+
+logout.addEventListener('click', function(){
+    loggedin = window.confirm('Are you sure you want to logout?');
+    if(loggedin == true){
+        output.innerText = 'You are now logged out. Click the login button to log in again.';
+        logout.style.display='none';
+        logout.setAttribute('disabled','disabled');
+        login.style.display='block';
+        login.removeAttribute('disabled');
+    }
+    else{
+        output.innerText = 'You cancelled the logout. You are still logged in.'; 
+    }
+});
---
+++ web-script-1/day-1/scripts/window-alert-method.js
@@ -0,0 +1,13 @@
+// Window Alert Method v1.0
+// by: Michael Whyte
+// c. 2019
+
+// Grab the button
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', function(){
+
+    // window.alert() or just alert() works as well
+    
+
+});
