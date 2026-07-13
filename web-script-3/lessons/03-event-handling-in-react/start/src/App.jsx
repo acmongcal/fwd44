a9e068da3801bf6b1405422d136b2d0173f47f39
Alan Christopher Mongcal <aranmongcal@gmail.com>
2026-07-13 09:40:03 -0700

    Added files for web script 3

---
+++ web-script-3/lessons/03-event-handling-in-react/start/src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css"
+
+/**
+ * Events in React
+ * 
+ * Create add, subtract, and reset functions that console.log a message
+ * when they are clicked.
+ */
+
+function App() {
+
+	return (
+		<>
+			<header className="App-header">
+				<h1>Events in React</h1>
+			</header>
+		</>
+	)
+}
+
+export default App
