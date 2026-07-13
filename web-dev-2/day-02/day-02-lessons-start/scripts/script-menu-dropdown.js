4dda4d943c40fe9776fbf9d2ecb93d0afefcfc34
Alan Christopher Mongcal <aranmongcal@gmail.com>
2026-07-13 09:41:16 -0700

    Added web dev 2 files

---
+++ web-dev-2/day-02/day-02-lessons-start/scripts/script-menu-dropdown.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+
+// Responsive Menu - Dropdown
+const body = document.body;
+const btnMenu = document.getElementById('btn-menu');
+const nav = document.getElementById('main-navigation');
+
+
+
+
+
+
