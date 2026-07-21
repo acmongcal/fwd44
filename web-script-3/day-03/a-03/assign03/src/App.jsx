4dad3b623408dc101491e2b73af41758202f3961
Alan Christopher Mongcal <aranmongcal@gmail.com>
2026-07-20 19:36:30 -0700

    Added progress for assignment 3 webscript 3

--- web-script-3/day-03/a-03/assign03/src/App.jsx
+++ web-script-3/day-03/a-03/assign03/src/App.jsx
@@ -1,32 +1,300 @@
-import './App.css'
+import "./App.css";
 import { useState } from "react";
-import {calculatorButtons} from "./globals/calculator-button-data";
-import Button from './components/Button';
-import { compute } from './utilities/utils';
-
+import { calculatorButtons } from "./globals/calculator-button-data";
 function App() {
-  const [currentValue, setCurrentValue] = useState("");
-  const [output, setOutput] = useState("");
-  function updateValue(button){
-    setCurrentValue(button.value + " : " + button.type);
-    setOutput(output+button.text);
+  const [arrayCalc, setArrayCalc] = useState([]);
+  const [memory, setMemory] = useState([]);
+
+  function compute(array) {
+    let expression = "";
+    array.map((exp, i) => {
+      if (exp.type === "number" && exp.sign === "-") {
+        expression = expression + (exp.sign + exp.value);
+      } else if (exp.type === "number" && exp.sign === "+") {
+        expression = expression + exp.value;
+      } else if (exp.type === "operator" && i != array.length - 1) {
+        if (exp.value === "Add") {
+          expression += "+";
+        } else if (exp.value === "Subtract") {
+          expression += "-";
+        } else if (exp.value === "Multiply") {
+          expression += "*";
+        } else if (exp.value === "Divide") {
+          expression += "/";
+        }
+      }
+    });
+    expression = expression.replaceAll("--","+");
+    return new Function("return " + expression)();
+  }
+
+  function computeArray(array) {
+    let newSign = "+";
+    let newVal = compute(array);
+    if (Math.sign(newVal) == -1) {
+      newSign = "-";
+      newVal *= -1;
+    }
+    return [
+      {
+        type: "number",
+        value: newVal,
+        prevOP: "memory",
+        sign: newSign,
+      },
+    ];
+  }
+
+  function updateNumber(newNumber, operation, sign = "+") {
+    const newArrayCalc = arrayCalc.map((e, i) => {
+      if (i === arrayCalc.length - 1) {
+        return {
+          type: "number",
+          value: newNumber,
+          prevOP: operation,
+          sign: sign,
+        };
+      } else {
+        return e;
+      }
+    });
+    setArrayCalc(newArrayCalc);
+  }
+
+  function removeCalc(index) {
+    setArrayCalc(arrayCalc.filter((e, i) => i !== index));
+  }
+  function validateOperations(input) {
+    const operations = ["operator", "sign", "decimal"];
+    switch (input.type) {
+      case "clear":
+        if (input.value === "All Clear") {
+          setArrayCalc([]);
+        } else {
+          removeCalc(arrayCalc.length - 1);
+        }
+        break;
+      case "enter":
+        if (arrayCalc.length != 0) {
+          setArrayCalc(computeArray(arrayCalc));
+        }
+        break;
+      case "operator":
+        if (
+          arrayCalc.length != 0 &&
+          !operations.includes(arrayCalc[arrayCalc.length - 1].type)
+        ) {
+          if (
+            input.value === "Percent" &&
+            arrayCalc[arrayCalc.length - 1].type === "number"
+          ) {
+            updateNumber(
+              arrayCalc[arrayCalc.length - 1].value / 100,
+              input.value,
+              arrayCalc[arrayCalc.length - 1].sign,
+            );
+          } else if (
+            input.value === "Square Root" &&
+            arrayCalc[arrayCalc.length - 1].type === "number"
+          ) {
+            updateNumber(
+              Math.sqrt(arrayCalc[arrayCalc.length - 1].value),
+              input.value,
+              arrayCalc[arrayCalc.length - 1].sign,
+            );
+          } else {
+            setArrayCalc([
+              ...arrayCalc,
+              { type: input.type, value: input.value },
+            ]);
+          }
+        }
+
+        break;
+      case "memory":
+        if (input.value === "Memory Clear") {
+          setMemory([]);
+        } else if (input.value === "Memory Recall") {
+          if (
+            arrayCalc.length != 0 &&
+            arrayCalc[arrayCalc.length - 1].type !== "number"
+          ) {
+            setArrayCalc([...arrayCalc, memory[0]]);
+          } else {
+            if (memory.length != 0) {
+              setArrayCalc([memory[0]]);
+            }
+          }
+        } else if (
+          memory.length != 0 &&
+          arrayCalc.length != 0 &&
+          arrayCalc[arrayCalc.length - 1].type === "number" &&
+          input.value === "Memory Addition"
+        ) {
+          console.log("Add memory");
+          const addMemory = [
+            ...memory,
+            {
+              type: "operator",
+              value: "Add",
+            },
+            arrayCalc[arrayCalc.length - 1],
+          ];
+          setMemory(computeArray(addMemory));
+        } else if (
+          memory.length != 0 &&
+          arrayCalc.length != 0 &&
+          arrayCalc[arrayCalc.length - 1].type === "number" &&
+          input.value === "Memory Subtract"
+        ) {
+          console.log("Minus memory");
+          const subtractMemory = [
+            ...memory,
+            {
+              type: "operator",
+              value: "Subtract",
+            },
+            arrayCalc[arrayCalc.length - 1],
+          ];
+          setMemory(computeArray(subtractMemory));
+        } else if (
+          arrayCalc.length != 0 &&
+          arrayCalc[arrayCalc.length - 1].type === "number" &&
+          input.value === "Memory Save"
+        ) {
+          const newMemory = {
+            type: arrayCalc[arrayCalc.length - 1].type,
+            value: arrayCalc[arrayCalc.length - 1].value,
+            prevOP: "memory",
+            sign: arrayCalc[arrayCalc.length - 1].sign,
+          };
+          setMemory([newMemory]);
+        }
+        break;
+      case "sign":
+        if (
+          arrayCalc.length != 0 &&
+          arrayCalc[arrayCalc.length - 1].type === "number" &&
+          arrayCalc[arrayCalc.length - 1].sign === "+"
+        ) {
+          console.log("Negative");
+          updateNumber(
+            arrayCalc[arrayCalc.length - 1].value,
+            arrayCalc[arrayCalc.length - 1].prevOP,
+            "-",
+          );
+        } else if (
+          arrayCalc.length != 0 &&
+          arrayCalc[arrayCalc.length - 1].type === "number" &&
+          arrayCalc[arrayCalc.length - 1].sign === "-"
+        ) {
+          console.log("Positive");
+          updateNumber(
+            arrayCalc[arrayCalc.length - 1].value,
+            arrayCalc[arrayCalc.length - 1].prevOP,
+            "+",
+          );
+        }
+        break;
+      case "decimal":
+        if (!String(arrayCalc[arrayCalc.length - 1].value).includes(".")) {
+          updateNumber(
+            arrayCalc[arrayCalc.length - 1].value + input.text,
+            "append",
+            arrayCalc[arrayCalc.length - 1].sign,
+          );
+        }
+        break;
+      case "number":
+        if (
+          arrayCalc.length != 0 &&
+          arrayCalc[arrayCalc.length - 1].type === "number" &&
+          arrayCalc[arrayCalc.length - 1].prevOP === "append"
+        ) {
+          updateNumber(
+            arrayCalc[arrayCalc.length - 1].value + "" + input.text,
+            "append",
+            arrayCalc[arrayCalc.length - 1].sign,
+          );
+        } else if (
+          arrayCalc.length != 0 &&
+          arrayCalc[arrayCalc.length - 1].type === "number" &&
+          arrayCalc[arrayCalc.length - 1].prevOP !== "append"
+        ) {
+          setArrayCalc([
+            {
+              type: input.type,
+              value: input.value,
+              prevOP: "append",
+              sign: "+",
+            },
+          ]);
+        } else {
+          setArrayCalc([
+            ...arrayCalc,
+            {
+              type: input.type,
+              value: input.value,
+              prevOP: "append",
+              sign: "+",
+            },
+          ]);
+        }
+        break;
+      default:
+        break;
+    }
+  }
+  function displayExp(exp) {
+    if (exp.type === "number" && exp.sign === "+") {
+      return exp.value;
+    } else if (exp.type === "number" && exp.sign === "-") {
+      return exp.sign + exp.value;
+    } else if (exp.type === "operator") {
+      if (exp.value === "Add") {
+        return " + ";
+      } else if (exp.value === "Subtract") {
+        return " - ";
+      } else if (exp.value === "Multiply") {
+        return " \u00d7 ";
+      } else if (exp.value === "Divide") {
+        return " \u00f7 ";
+      }
+    }
   }
   return (
     <div className="wrapper">
       <h1>Calculator App</h1>
-      <p>Calc: [ {currentValue} ]</p>
+      <h2>Pending calculation:</h2>
+      {arrayCalc.length > 0 && (
+        <ul>
+          {arrayCalc.map((calc, i) => (
+            <li key={i}>{displayExp(calc)}</li>
+          ))}
+        </ul>
+      )}
+      <p>
+        Output: [{" "}
+        {arrayCalc.length != 0 && arrayCalc[arrayCalc.length - 1].value} ]
+      </p>
       <section>
-        {calculatorButtons.map((button, i) => <Button     
-        buttonDetails={button}
-        key = {i}
-        updateValue = {updateValue}/>)
-        }
+        {calculatorButtons.map((button, i) => (
+          <button
+            className={button.className}
+            onClick={() => validateOperations(button)}
+            key={i}
+            value={button.value}
+          >
+            {button.text}
+          </button>
+        ))}
       </section>
-      <p>Current output: [{output}]</p>
-      <button onClick={()=>setOutput(compute("reset"))}>Output</button>
+      <p>
+        Memory:[
+        {memory.length != 0 && <b>{memory[0].sign + memory[0].value}</b>}]
+      </p>
     </div>
   );
-  
 }
 
 export default App;
--- web-script-3/day-03/a-03/assign03/src/utilities/utils.js
+++ web-script-3/day-03/a-03/assign03/src/utilities/utils.js
@@ -3,3 +3,4 @@ export const compute = function (string){
   return string;
 }
 
+
