c39f91fca473dc439fef33b6d4eb912406d12a7b
Alan Christopher Mongcal <aranmongcal@gmail.com>
2026-08-28 16:22:26 -0700

    removed typo in pagenotfound page

--- web-script-3/day-06/02-react-router-introduction/complete/src/pages/PageNotFound.jsx
+++ web-script-3/day-06/02-react-router-introduction/complete/src/pages/PageNotFound.jsx
@@ -12,7 +12,7 @@ const PageNotFound = () => {
     
 	return (
 		<section className="page-not-found-section">
-			<h2>404 ... : (</h2>
+			<h2>404 ... : </h2>
 			<p>Page not found.</p>
 			<p>Go to <Link to="/">Home</Link> page.</p>
 		</section>
